78d24852acc44a8d5b91d4e5e97fad5d9ead8b73
Zian Ke <kezian@umich.edu>
2018-11-08 15:31:52 -0500

    Init UDP from wSender to wReceiver

--- WTP-base/wReceiver.cpp
+++ WTP-base/wReceiver.cpp
@@ -15,6 +15,8 @@
 #include <time.h>
 #include <sys/time.h>
 
+#define MAXBUFLEN 2048
+
 int main(int argc, char *argv[]) {
     if (argc < 5) {
         std::cout << "Error: Usage is ./wReceiver <port_num> <log> <window_size> <file_dir>\n";
@@ -25,6 +27,42 @@ int main(int argc, char *argv[]) {
     char *log = argv[2];
     int window_size = atoi(argv[3]);
     char *file_dir = argv[4];
+
+    int sockfd;
+    struct sockaddr_in my_addr;    // my address information
+    struct sockaddr_in their_addr; // connector's address information
+    int addr_len, numbytes;
+    char buf[MAXBUFLEN];
+    bzero(buf, MAXBUFLEN);
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;         // host byte order
+    my_addr.sin_port = htons(port_num);     // short, network byte order
+    my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+    memset(&(my_addr.sin_zero), '\0', 8); // zero the rest of the struct
+
+    if (bind(sockfd, (struct sockaddr *) &my_addr,
+             sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    addr_len = sizeof(struct sockaddr);
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0,
+                             (struct sockaddr *) &their_addr, (socklen_t *) &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    printf("got packet from %s\n", inet_ntoa(their_addr.sin_addr));
+    printf("packet is %d bytes long\n", numbytes);
+    printf("packet contains \"%s\"\n", buf);
+
+    close(sockfd);
 
     return 0;
 }
--- WTP-base/wSender.cpp
+++ WTP-base/wSender.cpp
@@ -27,5 +27,37 @@ int main(int argc, char *argv[]) {
     char *receiver_IP = argv[4];
     int receiver_port = atoi(argv[5]);
 
+    int sockfd;
+    struct sockaddr_in their_addr; // connector's address information
+    struct hostent *he;
+    int numbytes;
+    char *message = "this is a test message";
+
+    if ((he = gethostbyname(receiver_IP)) == NULL) {  // get the host info
+        perror("gethostbyname");
+        exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    their_addr.sin_family = AF_INET;     // host byte order
+    their_addr.sin_port = htons(receiver_port); // short, network byte order
+    their_addr.sin_addr = *((struct in_addr *) he->h_addr);
+    memset(&(their_addr.sin_zero), '\0', 8); // zero the rest of the struct
+
+    if ((numbytes = sendto(sockfd, message, strlen(message), 0,
+                           (struct sockaddr *) &their_addr, sizeof(struct sockaddr))) == -1) {
+        perror("sendto");
+        exit(1);
+    }
+
+    printf("sent %d bytes to %s\n", numbytes,
+           inet_ntoa(their_addr.sin_addr));
+
+    close(sockfd);
+
     return 0;
 }
